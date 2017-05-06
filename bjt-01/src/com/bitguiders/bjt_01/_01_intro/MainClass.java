521f426b0770d4e0dd961a2f378825c2d925d512
bitguiders <bitguiders@bitguiders1-PC>
2017-05-06 15:58:29 -0500

    Simple modification in print statement

--- bjt-01/src/com/bitguiders/bjt_01/_01_intro/MainClass.java
+++ bjt-01/src/com/bitguiders/bjt_01/_01_intro/MainClass.java
@@ -3,7 +3,7 @@ package com.bitguiders.bjt_01._01_intro;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("My First Java Class");
+		System.out.println("My First Java Class.");
 	}
 
 }
