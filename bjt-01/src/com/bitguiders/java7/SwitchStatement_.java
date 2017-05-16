6fc225b3da7fc3ad366d738e51150c21d569966e
bitguiders <bitguiders@bitguiders1-PC>
2017-05-16 17:47:27 -0500

    Java 7 switch statement string usage example implemented

---
+++ bjt-01/src/com/bitguiders/java7/SwitchStatement_.java
@@ -0,0 +1,18 @@
+package com.bitguiders.java7;
+
+public class SwitchStatement_ {
+
+	public static void main(String[] args) {
+
+		String str = "two";
+		switch(str){
+		case "one":
+			System.out.println("You fond 1");
+			break;
+		case "two":
+			System.out.println("You fond 2");
+			break;
+		}
+	}
+
+}
