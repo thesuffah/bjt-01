82391eb9bfddc4c3a183336c72e66b3a45b71b12
Abdul Kareem <Abdul Kareem>
2017-08-05 18:22:50 -0400

    Chain pattern implementation

---
+++ bjt-01/src/com/bitguiders/design_patterns/chain_of_responsibility/FileLogger.java
@@ -0,0 +1,11 @@
+package com.bitguiders.design_patterns.chain_of_responsibility;
+
+public class FileLogger extends AbstractLogger {
+	public FileLogger(int level){
+		this.level = level;
+		}
+		@Override
+		protected void write(String message) {
+		System.out.println("File::Logger: " + message);
+		}
+}
